b91e8caaebb9890b7589688134915720ec93f2bd
Kevin Sheehan <kshhn12@gmail.com>
2020-03-02 12:55:03 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+Float first_number = 2;
+Float second_number = 1;
+
+Float sum = first_number + second_number;
+Float difference = first_number - second_number;
+Float product = first_number * second_number;
+Float quotient = first_number / second_number;
